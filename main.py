7367a41312500b909b7ed245431a35cda4ab3b53
Hassaan Qadir <hassaanqadir1@gmail.com>
2023-06-22 22:26:25 +0000

    new driver function w/ basic group and chain stuff

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
-from tasks import add, chain1, chain2
-import time
+from tasks import layer1, layer2, layer3
+from celery import group, chain
 
-result = add.delay(40, 6)
-print(result.ready())  # Should print False, task may not have completed.
 
-i = 0
-
-while i < 5:
-    print(i)
-    time.sleep(1)
-    i += 1
-    try:
-        print(result.get(timeout=1))  # Will print the answer if the task is done.
-    except:
-        print("Task not done yet.")
-    
-
-def driver(user_input):
+def driver1(user_input):
     output_string = ""
 
-    phase = chain1.delay(user_input).get() # Block until the task is done and get the result.
+    phase = layer1.delay(user_input).get() # Block until the task is done and get the result.
     
-    step1 = chain2.delay(phase, 1).get()  # Block until the task is done and get the result.
-    step2 = chain2.delay(phase, 2).get()  # Block until the task is done and get the result.
-    step3 = chain2.delay(phase, 3).get()  # Block until the task is done and get the result.
+    step1 = layer2.delay(phase, 1).get()  # Block until the task is done and get the result.
+    step2 = layer2.delay(phase, 2).get()  # Block until the task is done and get the result.
+    step3 = layer2.delay(phase, 3).get()  # Block until the task is done and get the result.
+
+    step4 = layer3.delay(step1, 4).get()  # Block until the task is done and get the result.
+    step5 = layer3.delay(step1, 5).get()  # Block until the task is done and get the result.
+    step6 = layer3.delay(step1, 6).get()  # Block until the task is done and get the result.
+
+    step7 = layer3.delay(step2, 7).get()  # Block until the task is done and get the result.
+    step8 = layer3.delay(step2, 8).get()  # Block until the task is done and get the result.
+    step9 = layer3.delay(step2, 9).get()  # Block until the task is done and get the result.
+
+    step10 = layer3.delay(step3, 10).get()  # Block until the task is done and get the result.
+    step11 = layer3.delay(step3, 11).get()  # Block until the task is done and get the result.
+    step12 = layer3.delay(step3, 12).get()  # Block until the task is done and get the result.
 
     output_string += str(phase)
     output_string += str(step1)
     output_string += str(step2)
     output_string += str(step3)
+    output_string += str(step4)
+    output_string += str(step5)
+    output_string += str(step6)
+    output_string += str(step7)
+    output_string += str(step8)
+    output_string += str(step9)
+    output_string += str(step10)
+    output_string += str(step11)
+    output_string += str(step12)
 
     print(output_string)
     return output_string
 
-driver("Make glow in the dark E. coli")
+
+def driver2(user_input):
+    output_string = ""
+
+    # Run layer1 task
+    phase = layer1.delay(user_input).get()  # Block until the task is done and get the result.
+
+    # Prepare a group of layer2 tasks, each chained with a group of layer3 tasks
+    g = group(
+        chain(layer2.s(phase, i), group(layer3.s(j) for j in range(1, 4)))
+        for i in range(1, 4)
+    )
+    
+    # Run the group of tasks and get the results
+    results = g.apply_async().get()
+    # Create the output string
+    output_string += str(phase)
+
+    for result in results:
+        output_string += str(result)
+    
+    print(output_string)
+    return output_string
+
+driver1("Make glow in the dark E. coli")
+
+
--- tasks.py
+++ tasks.py
@@ -3,30 +3,29 @@ import time
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 
-@app.task
-def add(x, y):
-    time.sleep(5)
-    return x + y + 100
-
 
 @app.task
-def chain1(user_input):
+def layer1(user_input):
     message = "Here is the user input: " + str(user_input) + "\n"
     message += "Let's break this into three phases\n"
 
     time.sleep(3)
 
-    message += "Phase 1 is Finding Genes\n"
-    message += "Phase 2 is Making a Plasmid\n"
-    message += "Phase 3 is Transforming E. coli\n"
-    message += "Let's break this phase into steps\n"
+    message += "Let's break each phase into steps\n"
 
     return message
 
 @app.task
-def chain2(phase, number):
-    message = "Chain #" + str(number) + " received : " + str(phase)
+def layer2(phase, number):
+    answer = "Phase #" + str(number) + " received : " + str(phase)[:10]
     time.sleep(3)
-    answer = "Returning Step #" + str(number) + "\n"
-    message += answer
-    return message
+    answer += "Returning Phase #" + str(number) + "\n\n"
+    return answer
+
+
+@app.task
+def layer3(phase, number):
+    answer = "Step #" + str(number) + " received : " + str(phase)[:10]
+    time.sleep(3)
+    answer += "Returning Step #" + str(number) + "\n\n"
+    return answer
