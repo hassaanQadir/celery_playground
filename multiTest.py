2d8fda438faafe21665a33041425a11ba2a0e837
Hassaan Qadir <hassaanqadir1@gmail.com>
2023-07-07 21:01:01 +0000

    new file w/ same OpenAI API calls but in sequence

--- multiTest.py
+++ multiTest.py
@@ -9,26 +9,6 @@ load_dotenv('.env')
 # Use the environment variables for the API keys if available
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Remember to set the OPENAI_API_KEY environment variable to your API key
-api = OpenAIMultiOrderedClient(endpoint="chats", data_template={"model": "gpt-3.5-turbo"})
-
-def make_requests():
-    for num in range(1, 3):
-        api.request(data={
-            "messages": [{
-                "role": "user",
-                "content": f"Can you tell me what is {num} * {num}? Incorporate the answer into a joke."
-            }]
-        }, metadata={'num': num})
-
-
-api.run_request_function(make_requests)
-
-for result in api:
-    num = result.metadata['num']
-    response = result.response['choices'][0]['message']['content']
-    print(f"{num} * {num}:", response)
-
 def stepMaker(phaseList):
     for phase in phaseList:
         api2.request(data={
